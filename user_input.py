7acf061c28edc9a826e35a9d9dcfcc0ea83f2292
Fred-045 <mwangifrederick773@gmail.com>
2024-08-14 05:58:42 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# user_input.py
+name = input("what is your name ")
+age = input("23 ")
+location = input("Nairobi")
+
+# Print a personalized message
+print("Hello {name}, you are {age} years old and live in {location}.")
